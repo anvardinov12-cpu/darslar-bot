2e49049aadd6daa2a3c3d989a6c3545ee1205ff6
anvardinov12-cpu <anvardinov12@gmail.com>
2026-09-07 14:10:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1238,11 +1238,11 @@ def main():
 
     app.job_queue.run_repeating(check_reminders, interval=60, first=5)
     app.job_queue.run_daily(
-    send_daily_schedule_job, 
-    time=datetime.strptime("12:50", "%H:%M").time(), 
-    days=(0, 1, 2, 3, 4, 5),
-    tz=TZ  # <--- Shu yerda vaqt mintaqasi ko'rsatiladi
-)
+        send_daily_schedule_job, 
+        time=datetime.strptime("12:50", "%H:%M").time(), 
+        days=(0, 1, 2, 3, 4, 5),
+        tz=TZ
+    )
 
     
     create_group_conv = ConversationHandler(
